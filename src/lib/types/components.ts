ebade3cd31c03f315c419e4c49ba4d395833179a
Rob Kendal <38687284+bpk68@users.noreply.github.com>
2024-07-11 11:01:08 +0100

    Updates call to actions to use asset type on Storyblok

--- src/lib/types/components.ts
+++ src/lib/types/components.ts
@@ -4,7 +4,7 @@ export type CallToActionContent = {
   title: string;
   intro: string;
   type: string;
-  download_item: string;
+  download_item: SbAsset;
   image: SbAsset;
   background_color_hex: string;
 };
